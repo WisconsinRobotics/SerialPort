471d0b1ea30364991a2480379df46c85d94c2381
William Jen <wjen@wisc.edu>
2016-11-13 18:17:22 -0600

    Disable read buffering.

--- SerialPort/SerialPort.cpp
+++ SerialPort/SerialPort.cpp
@@ -35,7 +35,7 @@ System::Boolean SerialPort::Open()
         0,
         0,
         OPEN_EXISTING,
-        FILE_FLAG_WRITE_THROUGH,
+        FILE_FLAG_WRITE_THROUGH | FILE_FLAG_NO_BUFFERING,
         0
     );
 
